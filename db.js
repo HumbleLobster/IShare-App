3b88802aab8a1865ae755a34cdd82c123c0c802d
aman <aman.ja943@gmail.com>
2021-05-16 13:17:03 +0530

    bugs

--- db.js
+++ db.js
@@ -5,15 +5,10 @@ dotenv.config();
 
 const client = new MongoClient(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true })
 client.connect(err => {
-    if(!err)
-    {
         module.exports = client;
         
         const app = require('./app');
 
         let port = process.env.PORT;
         app.listen(port);
-    }else{
-        console.log(err);
-    }
 });
